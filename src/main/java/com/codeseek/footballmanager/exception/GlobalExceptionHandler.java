d6c0b75074d97fec4d8aa2c5f836c3dce8ec366a
Zorii Bohdan <bodkazorii@gmail.com>
2025-01-24 14:07:54 +0200

    remove unused import

--- src/main/java/com/codeseek/footballmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeseek/footballmanager/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.codeseek.footballmanager.exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
